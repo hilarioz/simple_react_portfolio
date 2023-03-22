15f4a9609c802e9858d2f3111cf9d6a53698c855
hilarioz <hilariozubiria@gmail.com>
2023-03-22 18:33:04 -0300

    Update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,8 +36,8 @@ const Contact = () => {
             frameBorder={0}
             marginHeight={0}
             marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed/v1/place?q=La+Plata,+Provincia+de+Buenos+Aires,+Argentina&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            style={{ filter: "opacity(0.8)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=Instituto+Superior+de+Formación+Docente+y+Técnica+No.+12,+Avenida+7,+Villa+Elvira,+Provincia+de+Buenos+Aires,+Argentina&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
--- src/data.js
+++ src/data.js
@@ -32,14 +32,14 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "Quote",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
     name: "Andy Anderson",
     company: "Empresa_A",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "Quote",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
     name: "Bruno Diaz",
     company: "Empresa_B",
